7f15df6fe220e4311cb993722e9d958319a3dde3
XUHO3 <howells.xu@oocl.com>
2018-07-10 14:16:44 +0800

    practice 9 complete

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,45 @@
 package practice09;
 
 public class Klass {
+    int number;
+    Student leader;
+    public Klass(int number){
+        this.number=number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public String getDisplayName(){
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlassNumber()==number)
+        {
+            leader=student;
+        }
+        else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+
+    public void appendMember(Student student){
+        student.setKlassNumber(number);
+    }
 }
+
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,37 @@
 package practice09;
 
 public class Person {
+    int id;
+    String name;
+    int age;
+    public Person (int id,String name ,int age){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object person) {
+        return  this.id==((Person)person).id;
+    }
+
+    public  String introduce(){
+        return "My name is "+getName()+". I am "+getAge()+" years old.";
+    }
 }
